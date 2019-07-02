2024155a6e434765b48ada086772ad782df51b1a
Maksim Ashmetev <maksim_ashmetev@epam.com>
2019-07-02 10:53:01 +0300

    Component Card was added

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header />
     <SortPanel result="7" />
+    <Card name="Villi Vodka and an alcohol factory" genre="Action" year="2009" srcToImg="https://images-na.ssl-images-amazon.com/images/I/41dBu4DkbVL.jpg" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import SortPanel from "./components/SortPanel.vue";
+import Card from "./components/Card.vue";
 
 export default {
   name: "app",
   components: {
     Header,
-    SortPanel
+    SortPanel,
+    Card
   }
 };
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <img v-bind:src="srcToImg" class="poster"/>
+    <div>
+      <span class="posterName">{{ name }}</span>
+      <span class="year">{{ year }}</span>
+    </div>
+    <span class="genre">{{ genre }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,    
+    genre: String,
+    year: String,
+    srcToImg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  width: 20em;
+  flex-direction: column;
+  background: #6e6e6e;
+  color: #f4f4f4;
+  padding: 1em;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+  .poster {
+    width: 100%;
+    height: 25em;
+    margin-bottom: 2em;
+  }
+  .posterName {
+  }
+  .year {
+    padding: 0.2em 1em;
+    border: 2px solid #f4f4f4;
+    border-radius: 5px 5px 5px 5px;
+  }
+  .genre {
+  }
+}
+</style>
